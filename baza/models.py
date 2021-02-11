71a6c350f6827d8d54999d6c629d8db924587e34
cooklee <sbbogus@gmail.com>
2021-02-11 10:23:43 +0100

    add model Person

--- baza/models.py
+++ baza/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
